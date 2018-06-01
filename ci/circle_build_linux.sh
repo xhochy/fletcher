2313a0626a1556e430e84bfcd93cfd849ab70acc
Uwe L. Korn <uwelk@xhochy.com>
2018-06-01 19:49:20 +0200

    Build for Python 2 and 3

--- ci/circle_build_linux.sh
+++ ci/circle_build_linux.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
+export PYTHON_VERSION=$1
 export CONDA_PKGS_DIRS=$HOME/.conda_packages
 export MINICONDA=$HOME/miniconda
 export MINICONDA_URL="https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh"
@@ -16,7 +17,7 @@ conda config --set auto_update_conda false
 conda config --add channels https://repo.continuum.io/pkgs/free
 conda config --add channels conda-forge
 
-conda create -y -q -n fletcher python=3.6 \
+conda create -y -q -n fletcher python=${PYTHON_VERSION} \
     pandas pyarrow pytest pytest-cov \
     flake8 \
     pip \
