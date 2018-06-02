80097b3fc693dfcaa2430a7253eaa4f0c14d5325
Uwe L. Korn <uwelk@xhochy.com>
2018-06-02 09:42:44 +0200

    Add future imports to library code

--- fletcher/string_array.py
+++ fletcher/string_array.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from pandas.core.arrays import ExtensionArray
 from pandas.core.dtypes.dtypes import ExtensionDtype
 
