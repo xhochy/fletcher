0f249bcd6fb64aa59048003818c4fe65ed3862b5
Uwe L. Korn <uwe.korn@quantco.com>
2020-03-31 11:37:40 +0200

    Pull Arrow nightlies from arrow-nightlies

--- ci/circle_build_linux.sh
+++ ci/circle_build_linux.sh
@@ -43,7 +43,7 @@ fi
 
 if [ "${USE_DEV_WHEELS}" = "nightlies" ]; then
     echo "Installing Arrow dev"
-    conda install -c ursa-labs/label/nightly pyarrow arrow-cpp
+    conda install -c arrow-nightlies pyarrow arrow-cpp
     echo "Installing NumPy and Pandas dev"
     conda uninstall -y --force numpy pandas
     PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
