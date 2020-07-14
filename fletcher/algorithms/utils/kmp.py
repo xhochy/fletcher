ed353717e01866a9ddd7905d4c6ab471f7241957
Alexander Crustev <aleks.tcr@gmail.com>
2020-07-14 17:27:45 +0300

    Added some documentation for KMP functions

--- fletcher/algorithms/utils/kmp.py
+++ fletcher/algorithms/utils/kmp.py
@@ -46,7 +46,32 @@ def append_to_kmp_matching(
     character: int,
     pat: bytes,
     failure_function: np.ndarray
-) -> np.ndarray:
+) -> int:
+    """Append a character to a Knuth Moris Pratt matching.
+
+    This function can be used to search for `pat` in a text with the KMP
+    algorithm.
+
+    Parameters
+    ----------
+    matched_len: int
+        The length of the previous maximum prefix of `pat` that was a
+        suffix of the text. Must sattisfy `0 <= matched_len <= len(pat)`.
+    character: int
+        The next character of the text.
+    pat: bytes
+        The pattern that is searched in the text.
+    failure_function: np.ndarray
+        The KMP failure function of `pat`. Should be obtained through
+        `compute_kmp_failure_function(pat)`.
+
+    Returns
+    -------
+    int
+        The length of the maximum prefix of `pat` that is a suffix of the
+        text after appendng `character`. Always `=> 0` and
+        `<= min(matched_len + 1, len(pat))`.
+    """
     while matched_len > -1 and pat[matched_len] != character:
         matched_len = failure_function[matched_len]
     return matched_len + 1
