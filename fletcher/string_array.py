f7a53738d387fa9bbbc31af360a12d619fec4681
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2020-07-15 12:09:19 -0400

    docstring

--- fletcher/string_array.py
+++ fletcher/string_array.py
@@ -381,5 +381,5 @@ class TextAccessor:
         )
 
     def slice(self, start, end, step):
-        """Extract slice of strings from `start` to `end`."""
+        """Extract every `step` character from strings from `start` to `end`."""
         return self._series_like(_slice_handle_chunk(self.data, start, end, step))
