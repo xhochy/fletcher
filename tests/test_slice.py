8539aef0785d548f47b8c5745f567a6d41edbcad
Marc-Antoine Schmidt <marc-antoine.schmidt@quantco.com>
2020-07-15 14:43:08 -0400

    tests

--- tests/test_slice.py
+++ tests/test_slice.py
@@ -10,11 +10,11 @@ import fletcher as fr
         (["abcd", "defg", "hijk"], (1, 2, 1), ["b", "e", "i"]),
         (["abcd", "defg", "h"], (1, 2, 1), ["b", "e", ""]),
         (["abcd", "defg", "hijk"], (1, 4, 2), ["bd", "eg", "ik"]),
-        (["abcd", "defg", "hijk"], (0, -2, 1), ["ab", "de", "hi"]),  # not working
-        (["abcd", "defg", "hijk"], (-5, -2, 1), ["ab", "de", "hi"]),  # not working
+        (["abcd", "defg", "hijk"], (0, -2, 1), ["ab", "de", "hi"]),
+        (["abcd", "defg", "hijk"], (-5, -2, 1), ["ab", "de", "hi"]),
         (["aécd", "d🙂fg", "écijk"], (1, 2, 1), ["é", "🙂", "c"]),
-        (["abcd", "defg", "hijk"], (3, 1, -1), ["cb", "fe", "ji"]),  # not working
-        (["abcd", "defg", "hijk"], (5, 0, -2), ["db", "ge", "ki"]),  # not working
+        (["abcd", "defg", "hijk"], (3, 1, -1), ["cb", "fe", "ji"]),
+        (["abcd", "defg", "hijk"], (5, 0, -2), ["db", "ge", "ki"]),
         (["abcd", "defg", "hijk"], (3, 20, 1), ["d", "g", "k"]),
         (["abcd", "defg", "hijk"], (10, 20, 1), ["", "", ""]),
         (["abcd", "defg", None], (10, 20, 1), ["", "", None]),
@@ -25,11 +25,6 @@ import fletcher as fr
     "storage_type", [fr.FletcherContinuousArray, fr.FletcherChunkedArray]
 )
 def test_slice(data, slice_, expected, storage_type):
-    """
-    - test negative slice
-    - test step
-    - test negative step
-    """
     fr_series = pd.Series(storage_type(data))
     fr_out = fr_series.text.slice(*slice_).astype(object)
     pd.testing.assert_series_equal(fr_out, pd.Series(expected))
